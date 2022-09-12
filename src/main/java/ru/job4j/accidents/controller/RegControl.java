a856d04a059d4756dc04f4d62e35825f6e464bd2
nikisha-script <nikisha_script@mail.ru>
2022-09-12 08:02:22 +0300

    2. Регистрация пользователя [#296069 #341691]

--- src/main/java/ru/job4j/accidents/controller/RegControl.java
+++ src/main/java/ru/job4j/accidents/controller/RegControl.java
@@ -1,13 +1,18 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.dataspring.AuthorityRepository;
 import ru.job4j.accidents.repository.dataspring.UserRepository;
+
+import java.util.Optional;
 
 @Controller
 public class RegControl {
@@ -22,18 +27,27 @@ public class RegControl {
         this.authorities = authorities;
     }
 
+    @GetMapping("/reg")
+    public String regPage(Model model, @RequestParam(name = "error", required = false) String errorMessage) {
+        String errorMsg = null;
+        if (errorMessage != null) {
+            errorMsg = "Пользователь с таким ником уже существует";
+        }
+        model.addAttribute("errorMessage", errorMsg);
+        return "reg";
+    }
+
     @PostMapping("/reg")
     public String regSave(@ModelAttribute User user) {
+        Optional<User> userDb = users.findByUsername(user.getUsername());
+        if (userDb.isPresent()) {
+            return "redirect:/reg?error=true";
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
         users.save(user);
         return "redirect:/login";
-    }
-
-    @GetMapping("/reg")
-    public String regPage() {
-        return "reg";
     }
 
 }
--- src/main/java/ru/job4j/accidents/repository/dataspring/UserRepository.java
+++ src/main/java/ru/job4j/accidents/repository/dataspring/UserRepository.java
@@ -3,5 +3,8 @@ package ru.job4j.accidents.repository.dataspring;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accidents.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
